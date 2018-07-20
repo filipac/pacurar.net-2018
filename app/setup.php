aa7542ff7e4e69a72516f417da63be1dcba33df2
Filip Pacurar <filip@pacurar.net>
2018-07-21 02:51:45 +0300

    1.0.3

--- app/setup.php
+++ app/setup.php
@@ -12,8 +12,8 @@ use Roots\Sage\Template\BladeProvider;
  */
 add_action(
     'wp_enqueue_scripts', function () {
-        wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, '1.0.2');
-        wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], '1.0.2', true);
+        wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, '1.0.3');
+        wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], '1.0.3', true);
     }, 1
 );
 
