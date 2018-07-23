66e9c41336796502186ed75ce3e38052f5530130
Filip Pacurar <filip@pacurar.net>
2018-07-23 15:59:02 +0300

    another

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
     @php dynamic_sidebar('sidebar-footer') @endphp
   </div>
 
-  <div class="container">
+  <div class="container mx-auto">
     <div class="copy footer">
       <p>&copy; Filip Păcurar {{ date('Y') }} | Made with love and <a href="https://tailwindcss.com/" target='_blank'>Tailwind CSS</a>.</p>
     </div>
