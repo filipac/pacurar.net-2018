6a1248ab1dadc2cff53da0b93a8c088281a6e7a0
Filip Pacurar <filip@pacurar.net>
2018-07-24 10:02:22 +0300

    analyitics code

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -9,3 +9,12 @@
     </div>
   </div>
 </footer>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-53615822-4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-53615822-4');
+</script>
