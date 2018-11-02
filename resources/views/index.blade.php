47ca1aaa478a829991a58b6e7dbbe4636972cee0
Filip Pacurar <filip@pacurar.net>
2018-11-03 01:40:30 +0200

    a little fix for mobile

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,14 +25,14 @@ $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
         @else
           @include('partials.content-'.get_post_type())
           @if($paged == 1 && is_home() && $count == 1)
-            <article class="bg-white mt-12 flex flex-row sm:flex-col min-h-0 podcast-div sm:p-12" style="background-image: url('@asset('images/bg_podcast.jpg')');     background-size: cover;">
+            <article class="bg-white mt-12 flex flex-row sm:flex-col min-h-0 podcast-div sm:p-6" style="background-image: url('@asset('images/bg_podcast.jpg')');     background-size: cover;">
                 <img src="@asset('images/cover_podcast.jpg')" alt="Podcast Catalina si Filip Pacurar - Cu de toate" class="podcast-image  sm:max-w-full sm:w-full" />
-                <div class="pl-4 pt-4">
+                <div class="pl-4 pt-4 sm:pl-0">
                     <a href="https://anchor.fm/cudetoate" target="_blank"><h1 class="text-white">Cu de toate - Podcast-ul nostru</h1></a>
                     <div class="text-grey text-base ml-1">By Filip si Catalina Pacurar</div>
                     <div class="text-white ml-1 mt-4">Cu de toate... exact ca o shaorma! Podcastul unui programator in care voi vorbi despre orice imi trece prin minte! Absolut orice... deci nu doare de programare :D</div>
                     <div>
-                        <iframe src="https://anchor.fm/cudetoate/embed" height="102px" width="400px" frameborder="0" scrolling="no"></iframe>
+                        <iframe src="https://anchor.fm/cudetoate/embed" height="102px" width="325px" frameborder="0" scrolling="no"></iframe>
                     </div>
                     <div class="mt-2 flex flex-row">
                         <div><a href="https://anchor.fm/cudetoate" target="_blank"><img src="@asset('images/anchor.png')" style="width: 200px" alt=""></a></div>
